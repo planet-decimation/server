7bab00098d2d4de4f3c2827e88ec49503c531ef4
Nathan Gawith <NathanGawith@gmail.com>
2018-10-19 22:26:37 -0500

    WEB API
    made the project into an actual web api

--- server/Program.cs
+++ server/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SampleWebApiAspNetCore.Repositories;
+using SampleWebApiAspNetCore.Services;
+using System;
 
-namespace server
+namespace WebApplication11
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = CreateWebHostBuilder(args).Build();
+
+            // Initializes db.
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<FoodDbContext>();
+                    var dbInitializer = services.GetRequiredService<ISeedDataService>();
+                    dbInitializer.Initialize(context).GetAwaiter().GetResult();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
